90119dfefab6b88bdc2ac40d23c49cdfefa73138
KenasiaTurner <kenasi123@gmail.com>
2022-05-13 09:23:37 -0400

    adds the changes/ components

---
+++ src/components/Counter.js
@@ -0,0 +1,40 @@
+import React from 'react'; 
+
+import Output from './Output';
+import Increment from './Increment';
+import Decrement from './Decrement';
+
+class Counter extends React.Component { 
+    constructor(props) {
+        super(props);
+        this.state = {
+            count:0 
+        };
+    }
+    
+    render() {
+        return (
+            <div>
+                <Decrement handleClick={this._decrementCounter} />
+                <Output count={this.state.count} />
+                <Increment handleClick={this._incrementCounter} />
+            </div>
+        );
+    }
+    _incrementCounter = () => {
+        // Call this.setState() to increment this.state.count
+        this.setState({
+            count: this.state.count + 1
+        }, () => { 
+            console.log('Finished updating the count');})
+    } 
+    _decrementCounter = () => {
+        this.setState({
+            count: this.state.count - 1
+        }, () => {
+            console.log('Finished updating the count');
+        })
+    }
+}
+
+export default Counter;
